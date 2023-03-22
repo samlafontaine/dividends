6a525c568e7e32fff1fd056d0d96b27c1b39f7cb
Sam Lafontaine <sam@samlafontaine.com>
2023-03-22 14:31:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,61 +3,61 @@ const tickerInput = document.getElementById('ticker');
 const resultsDiv = document.getElementById('results');
 
 tickerInput.addEventListener('keydown', (event) => {
-    if (event.keyCode === 13) {
-        event.preventDefault(); // Prevent the default action of submitting the form
-        submitButton.click(); // Trigger the submit button click event
-    }
+  if (event.keyCode === 13) {
+    event.preventDefault(); // Prevent the default action of submitting the form
+    submitButton.click(); // Trigger the submit button click event
+  }
 });
 
 
 submitButton.addEventListener('click', () => {
-    const apiKey = 'YOUR_API_KEY_HERE';
-    const ticker = tickerInput.value;
-    const apiUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=${ticker}&apikey=${apiKey}`;
-    const table = document.createElement('table');
+  const apiKey = 'YOUR_API_KEY_HERE';
+  const ticker = tickerInput.value;
+  const apiUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=${ticker}&apikey=${apiKey}`;
+  const table = document.createElement('table');
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Parse the JSON response to extract the dividend history data
-            const dividends = [];
-            for (let date in data['Monthly Adjusted Time Series']) {
-                if (data['Monthly Adjusted Time Series'][date]['7. dividend amount']) {
-                    dividends.push({
-                        date: date,
-                        dividend: data['Monthly Adjusted Time Series'][date]['7. dividend amount'],
-                    });
-                }
-            }
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Parse the JSON response to extract the dividend history data
+      const dividends = [];
+      for (let date in data['Monthly Adjusted Time Series']) {
+        if (data['Monthly Adjusted Time Series'][date]['7. dividend amount']) {
+          dividends.push({
+            date: date,
+            dividend: data['Monthly Adjusted Time Series'][date]['7. dividend amount'],
+          });
+        }
+      }
 
-            // Group dividends by year
-            const dividendsByYear = {};
-            dividends.forEach(dividend => {
-                const year = dividend.date.substring(0, 4);
-                if (!dividendsByYear[year]) {
-                    dividendsByYear[year] = [];
-                }
-                dividendsByYear[year].push(dividend);
-            });
+      // Group dividends by year
+      const dividendsByYear = {};
+      dividends.forEach(dividend => {
+        const year = dividend.date.substring(0, 4);
+        if (!dividendsByYear[year]) {
+          dividendsByYear[year] = [];
+        }
+        dividendsByYear[year].push(dividend);
+      });
 
-            // Sort the years in descending order
-            const years = Object.keys(dividendsByYear).sort((a, b) => b - a);
+      // Sort the years in descending order
+      const years = Object.keys(dividendsByYear).sort((a, b) => b - a);
+    
+      // Calculate the total dividends and variation for each year
+      const yearlyTotals = {};
+      const yearlyVariations = {};
+      years.forEach((year, index) => {
+        const yearlyDividends = dividendsByYear[year];
+        yearlyTotals[year] = yearlyDividends.reduce((total, dividend) => total + parseFloat(dividend.dividend), 0);
 
-            // Calculate the total dividends and variation for each year
-            const yearlyTotals = {};
-            const yearlyVariations = {};
-            years.forEach((year, index) => {
-                const yearlyDividends = dividendsByYear[year];
-                yearlyTotals[year] = yearlyDividends.reduce((total, dividend) => total + parseFloat(dividend.dividend), 0);
+        if (index > 0) {
+          const previousYear = years[index - 1];
+          yearlyVariations[previousYear] = ((yearlyTotals[previousYear] - yearlyTotals[year])/yearlyTotals[year])*100;
+        }
+      });
 
-                if (index > 0) {
-                    const previousYear = years[index - 1];
-                    yearlyVariations[previousYear] = ((yearlyTotals[previousYear] - yearlyTotals[year]) / yearlyTotals[year]) * 100;
-                }
-            });
-
-            // Create the table
-            table.innerHTML = `
+      // Create the table
+table.innerHTML = `
     <thead>
       <tr>
         <th>Year</th>
@@ -85,24 +85,20 @@ submitButton.addEventListener('click', () => {
             const month = (i + 1).toString().padStart(2, '0');
             const dividend = dividendsByYear[year].find(dividend => dividend.date.substring(5, 7) === month);
             return `<td>${dividend && dividend.dividend !== '0.0000' ? dividend.dividend : '-'}</td>`;
-        }).join('')
-} <
-td > $ {
-    yearlyTotals[year].toFixed(4)
-} < /td> <
-td > $ {
-    yearlyVariations[year] ? yearlyVariations[year].toFixed(2) + '%' : '-'
-} < /td> <
-/tr>
-`).join('')}
+          }).join('')}
+          <td>${yearlyTotals[year].toFixed(4)}</td>
+          <td>${yearlyVariations[year] ? yearlyVariations[year].toFixed(2) + '%' : '-'}</td>
+        </tr>
+      `).join('')}
     </tbody>
   `;
 
-// Display the table in the results div
-resultsDiv.innerHTML = ''; resultsDiv.appendChild(table);
-})
-.catch(error => {
-console.error(error);
-resultsDiv.textContent = 'An error occurred while retrieving the dividend history data';
-});
+      // Display the table in the results div
+      resultsDiv.innerHTML = '';
+      resultsDiv.appendChild(table);
+    })
+    .catch(error => {
+      console.error(error);
+      resultsDiv.textContent = 'An error occurred while retrieving the dividend history data';
+    });
 });
